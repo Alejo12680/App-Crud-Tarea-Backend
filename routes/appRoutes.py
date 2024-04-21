4da155c413e22a68cf0afeca6fbe070fe753e5bf
Alejo12680 <alejo152725@gmail.com>
2024-04-21 02:37:29 -0500

    creacion de servicios

--- routes/appRoutes.py
+++ routes/appRoutes.py
@@ -2,6 +2,8 @@ from flask import Blueprint, request, jsonify
 from databases.db_mysql import get_connection
 from utils.webToken import crearToken, validarToken
 from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
+from pymysql import err
 
 routes_auth = Blueprint('routes_auth', __name__)
 
@@ -19,7 +21,8 @@ def loginRoute():
     cur = conn.cursor()
     # generamos la consulta
     cur.execute(
-        "SELECT * FROM tb_usuario_register WHERE correo = '{}' and tb_usuario_register.estado = 'Activo'".format(correo))
+        "SELECT * FROM tb_usuario_register WHERE correo = '{}' and tb_usuario_register.estado = 'Activo'".format(
+            correo))
 
     result = cur.fetchone()
 
@@ -48,10 +51,76 @@ def loginRoute():
         # pasamos de tupla a diccionario
         dataParaToken: dict = {
 
-            'id': result[0],
-            'nombre': result[1],
-            'correo': result[2],
+            'id': result[0], 'nombre': result[1], 'correo': result[2],
 
         }
 
         return crearToken(dataParaToken)
+
+
+@routes_auth.route('/verificarToken', methods=['GET'])
+def verificarToken():
+    token = request.headers['Authorization'].split(" ")[1]
+
+    validacion = validarToken(token, False)
+
+    if validacion:
+        return jsonify({"message": "Token success", "Authorization": True})
+
+    else:
+        return validacion
+
+
+@routes_auth.route('/register', methods=['POST'])
+def register():
+    dat = request.get_json()
+    nombre = dat['nombre']
+    correo = dat['correo']
+    contrasena = dat['contrasena']
+
+    token = request.headers['Authorization'].split(" ")[1]
+    validacion = validarToken(token, False)
+
+    ## si el token es valido
+    if validacion:
+
+        datosToken = validarToken(token, True)
+
+        try:
+            fecha_creacion = datetime.now()
+            nombre = dat['nombre']
+            correo = dat['correo']
+            contra_encriptado = generate_password_hash(dat['contraseña'], 'pbkdf2:sha256', 30)
+            estado = 'Activo'
+
+            conn = get_connection()
+            cur = conn.cursor()
+
+            print(fecha_creacion)
+
+            cur.execute("""INSERT INTO tb_usuario_register (nombre, correo, contrasena, fecha_creacion, estado) VALUES ('{}', '{}', '{}', '{}', '{}' )""".format(nombre, correo, contra_encriptado, fecha_creacion, estado))
+
+            # cerramos la conexion a la bd
+            conn.commit()
+            cur.close()
+            conn.close()
+
+            return jsonify({"message": "El usuario se ha creado con exito"})
+
+        except err.IntegrityError as er:
+
+            # organizamos y limpiamos el error
+            error = format(er).replace('(', '').replace(')', '').replace('/', '').replace('"', '').split(',')
+
+            if error[0] == '1062':
+                response = jsonify({'message': 'Este usuario ya existe', 'numErro': error[0]})
+                response.status_code = 400
+                return response
+
+            ## Quitamos caracteres innecesarios
+            response = jsonify({'message': error[1], 'numErro': error[0]})
+            response.status_code = 400
+            return response
+
+    else:
+        return validacion
