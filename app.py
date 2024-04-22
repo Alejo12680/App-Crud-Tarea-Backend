31e5fb864ba8c6e6398ba8b93669bc707c5a8680
Alejo12680 <alejo152725@gmail.com>
2024-04-22 10:27:10 -0500

    Agrego_CORS_Funcionamiento

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from routes.appRoutes import routes_auth
-from dotenv import load_dotenv
+from flask_cors import CORS
+
 
 app = Flask(__name__)
 app.register_blueprint(routes_auth)
+CORS(app)
 
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000, use_reloader=False)
-    load_dotenv()
+
